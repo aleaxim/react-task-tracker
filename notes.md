d7b92ef49469f12b70f08c9c0896d99b74b8e47b
Aleaxim <patriciamichaela@outlook.com>
2022-05-02 00:03:59 +0800

    Added simpe array data

--- notes.md
+++ notes.md
@@ -16,6 +16,7 @@
     * One-way data binding, can't mutate directly 
      
 
+# Coding in React 
      <></> or <div></div>
 
      actions get passed down, state gets passed up
@@ -24,3 +25,14 @@
      With backend, we'll be making http or fetch request
 
      Use && to make a ternary without else 
+
+# Build for Production 
+
+    npm run build
+    - build folder containing static assets, what u push for production 
+    
+    to deploy locally 
+        sudo npm i -g serve         this will install globally
+        serve -s build -p 8000      made up port 
+    ctrl + c to stop 
+    
--- src/App.js
+++ src/App.js
@@ -10,7 +10,20 @@ import AddTask from './components/AddTask'
 const App = () => {
   // Global state
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([
+      {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "Feb 5th at 2:30pm",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "Meeting at School",
+        "day": "Feb 6th at 1:30pm",
+        "reminder": true
+      }
+  ])
 
   // Add Task
   const addTask = (task) => {
@@ -54,7 +67,7 @@ const App = () => {
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       ) : (
-        'You got no task.'
+        'You have no tasks'
       )}
     </div>
   )
